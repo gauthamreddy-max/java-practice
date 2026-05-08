47eeb238b56bae070f2120df5b23d2933b0e5491
gauthamreddy-max <gauthamreddyguddeti821@gmail.com>
2026-05-08 10:53:33 -0500

    abstract classs revesion

---
+++ AbstractBank.java
@@ -0,0 +1,11 @@
+ public abstract class AbstractBank{
+    abstract void savings();
+    abstract void current();
+    abstract void salary();
+    abstract void joint();
+    public void branchDetails(){
+        System.out.println("chennai");
+    }
+
+ }
+ //rest of code in axis bank
